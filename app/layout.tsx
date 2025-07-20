4800e76984f142179e4fde097bdd67f22a0b475d
Augusto Boschiazzo Marinelli <augus2608@gmail.com>
2025-07-20 13:42:19 -0300

    fix font variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import { Toaster } from "sonner";
 
 export const font = Gabarito({
+    variable: "--font-gabarito",
     subsets: ["latin"],
     weight: "400",
 });
@@ -21,8 +22,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" className={font.className}>
-            <body>
+        <html lang="en">
+            <body className={`${font.variable} `}>
                 <Navbar />
                 <main className="min-h-screen w-full mx-auto px-2 lg:px-0">
                     {children}
