a182bf5ec7d5fee472d107d2e7ca91fe9b22e62a
Augusto Boschiazzo Marinelli <augus2608@gmail.com>
2025-07-20 13:53:43 -0300

    fix font export in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import { Toaster } from "sonner";
 
-export const gabarito = Gabarito({
+const gabarito = Gabarito({
     variable: "--font-gabarito",
     subsets: ["latin"],
     weight: "400",
